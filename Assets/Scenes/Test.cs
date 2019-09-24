1036cdd78d439b9b1326108a57325b770e8cd142
sobernaut <silwalujjwal@gmail.com>
2019-09-24 17:47:51 +0545

    third test

---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("wasssup");
+        print("wasssup");
+        print("wasssup");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
